b5ebe1d99ffa9730cf4edf072dc559099881cef7
Sunil Padmanabhan <psunil@corelogic.com>
2016-06-13 16:13:00 -0700

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PATH="/root/.rbenv/bin:$PATH" && \
+export PATH="/home/builder/.rbenv/bin:$PATH" && \
 export RUBY_CFLAGS='-O2' && \
 export CONFIGURE_OPTS="--disable-install-doc" && \
 eval "$(rbenv init -)"
