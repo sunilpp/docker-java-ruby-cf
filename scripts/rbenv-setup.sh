8b1193b0d5bb49315ed44022776ff780bd47ba9a
Sunil Padmanabhan <psunil@corelogic.com>
2016-06-14 17:57:20 -0700

    Update rbenv-setup.sh

--- scripts/rbenv-setup.sh
+++ scripts/rbenv-setup.sh
@@ -4,6 +4,10 @@ git clone --depth 1 https://github.com/sstephenson/rbenv.git /home/builder/.rben
 git clone --depth 1 https://github.com/sstephenson/ruby-build.git /home/builder/.rbenv/plugins/ruby-build && \
 rm -rfv /home/builder/.rbenv/plugins/ruby-build/.git && \
 rm -rfv /home/builder/.rbenv/.git && \
+mkdir -p /home/builder/.rbenv/versions && \
+mkdir -p /home/builder/.rbenv/shims && \
+chown -R builder:builder /home/builder/.rbenv/versions && \
+chown -R builder:builder /home/builder/.rbenv/shims && \
 export PATH="/home/builder/.rbenv/bin:/home/builder/.rbenv/plugins/ruby-build/bin:$PATH" && \
 export RUBY_CFLAGS='-O2' && \
 export CONFIGURE_OPTS="--disable-install-doc" && \
@@ -12,4 +16,3 @@ rbenv install $1 && \
 rbenv global $1 && \
 gem install bundler && \
 rbenv rehash
-
